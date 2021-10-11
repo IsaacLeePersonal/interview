ee9b276375aa33986f805c4447a8b4efb761bbed
Isaac Lee <ielhome253@gmail.com>
2021-10-11 14:59:56 +0100

    initial

---
+++ variables-and-operators.m
@@ -0,0 +1,34 @@
+% This is where the header to the file would be.
+% Instructions for the code file would be here.
+
+% You can assign variables with the = sign
+
+a = 2;
+
+b = 14;
+
+% Rules of naming variables:
+% - must begin with a letter
+% - must contain only letters, numbers, or underscores.
+% Variables don't need to be named according to their purpose but it helps if you do so, for example:
+
+a = 378;
+b = 60;
+
+c = a/b;
+
+disp(c);
+
+% does exactly the same thing as:
+
+cars_spotted = 378;
+
+minutes = 60;
+
+rate = cars_spotted/minutes;
+
+disp(rate);
+
+% In both cases the computer reads the scripts in exactly the same way, but in the latter it's easier for the user to read it, so this is preferable.
+
+%% ACTIVITIES
